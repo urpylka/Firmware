502474c84eb864f0723ec9fb646154b2fec0a564
elia <elias.tarasov@gmail.com>
2017-07-05 21:54:25 +0300

    Changed I2C bus rate to 100KHz (nominal).

--- src/drivers/vl53l0x/vl53l0x.cpp
+++ src/drivers/vl53l0x/vl53l0x.cpp
@@ -281,7 +281,7 @@ private:
 extern "C" __EXPORT int vl53l0x_main(int argc, char *argv[]);
 
 VL53L0X::VL53L0X(int bus, int address) :
-	I2C("VL53L0X", VL53L0X_DEVICE_PATH, bus, address, 400000),
+	I2C("VL53L0X", VL53L0X_DEVICE_PATH, bus, address, 100000),
 	_min_distance(-1.0f),
 	_max_distance(-1.0f),
 	_conversion_interval(-1),
@@ -377,212 +377,11 @@ VL53L0X::init()
 
 	if (_distance_sensor_topic == nullptr) {
 		DEVICE_LOG("failed to create distance_sensor object. Did you start uOrb?");
+		return ret;
 	}
 
-	// read WHO_AM_I register
-	uint8_t I_AM = readReg(VL53L0X_WHO_AM_I);
-	if(I_AM == 0xEE) {
-		// "Set I2C standard mode"
-		writeReg(0x88, 0x00);
-		writeReg(0x80, 0x01);
-		writeReg(0xFF, 0x01);
-		writeReg(0x00, 0x00);
-		stop_variable = readReg(0x91);
-		writeReg(0x01, 0x00);
-		writeReg(0xFF, 0x00);
-		writeReg(0x80, 0x00);
+	ret = PX4_OK;
 
-		// disable SIGNAL_RATE_MSRC (bit 1) and SIGNAL_RATE_PRE_RANGE (bit 4) limit checks
-		writeReg(MSRC_CONFIG_CONTROL, readReg(MSRC_CONFIG_CONTROL) | 0x12);
-
-		setSignalRateLimit(0.1);
-		setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-		setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
-
-		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xFF);
-
-		uint8_t spad_count;
-		bool spad_type_is_aperture;
-		if (!getSpadInfo(&spad_count, &spad_type_is_aperture)) {
-			printf("No spad info\n");
-			return false;
-		}
-
-		// The SPAD map (RefGoodSpadMap) is read by VL53L0X_get_info_from_device() in
-		// the API, but the same data seems to be more easily readable from
-		// GLOBAL_CONFIG_SPAD_ENABLES_REF_0 through _6, so read it from there
-		uint8_t ref_spad_map[6];
-		readMulti(GLOBAL_CONFIG_SPAD_ENABLES_REF_0, ref_spad_map, 6);
-
-		// -- VL53L0X_set_reference_spads() begin (assume NVM values are valid)
-
-		writeReg(0xFF, 0x01);
-		writeReg(DYNAMIC_SPAD_REF_EN_START_OFFSET, 0x00);
-		writeReg(DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD, 0x2C);
-		writeReg(0xFF, 0x00);
-		writeReg(GLOBAL_CONFIG_REF_EN_START_SELECT, 0xB4);
-
-		uint8_t first_spad_to_enable = spad_type_is_aperture ? 12 : 0; // 12 is the first aperture spad
-		uint8_t spads_enabled = 0;
-
-		for (uint8_t i = 0; i < 48; i++) {
-			if (i < first_spad_to_enable || spads_enabled == spad_count) {
-		      // This bit is lower than the first one that should be enabled, or
-		      // (reference_spad_count) bits have already been enabled, so zero this bit
-		      ref_spad_map[i / 8] &= ~(1 << (i % 8));
-		    }
-		    else if ((ref_spad_map[i / 8] >> (i % 8)) & 0x1)
-		    {
-		      spads_enabled++;
-		    }
-		}
-
-		writeMulti(GLOBAL_CONFIG_SPAD_ENABLES_REF_0, ref_spad_map, 6);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x00, 0x00);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x09, 0x00);
-		writeReg(0x10, 0x00);
-		writeReg(0x11, 0x00);
-
-		writeReg(0x24, 0x01);
-		writeReg(0x25, 0xFF);
-		writeReg(0x75, 0x00);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x4E, 0x2C);
-		writeReg(0x48, 0x00);
-		writeReg(0x30, 0x20);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x30, 0x09);
-		writeReg(0x54, 0x00);
-		writeReg(0x31, 0x04);
-		writeReg(0x32, 0x03);
-		writeReg(0x40, 0x83);
-		writeReg(0x46, 0x25);
-		writeReg(0x60, 0x00);
-		writeReg(0x27, 0x00);
-		writeReg(0x50, 0x06);
-		writeReg(0x51, 0x00);
-		writeReg(0x52, 0x96);
-		writeReg(0x56, 0x08);
-		writeReg(0x57, 0x30);
-		writeReg(0x61, 0x00);
-		writeReg(0x62, 0x00);
-		writeReg(0x64, 0x00);
-		writeReg(0x65, 0x00);
-		writeReg(0x66, 0xA0);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x22, 0x32);
-		writeReg(0x47, 0x14);
-		writeReg(0x49, 0xFF);
-		writeReg(0x4A, 0x00);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x7A, 0x0A);
-		writeReg(0x7B, 0x00);
-		writeReg(0x78, 0x21);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x23, 0x34);
-		writeReg(0x42, 0x00);
-		writeReg(0x44, 0xFF);
-		writeReg(0x45, 0x26);
-		writeReg(0x46, 0x05);
-		writeReg(0x40, 0x40);
-		writeReg(0x0E, 0x06);
-		writeReg(0x20, 0x1A);
-		writeReg(0x43, 0x40);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x34, 0x03);
-		writeReg(0x35, 0x44);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x31, 0x04);
-		writeReg(0x4B, 0x09);
-		writeReg(0x4C, 0x05);
-		writeReg(0x4D, 0x04);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x44, 0x00);
-		writeReg(0x45, 0x20);
-		writeReg(0x47, 0x08);
-		writeReg(0x48, 0x28);
-		writeReg(0x67, 0x00);
-		writeReg(0x70, 0x04);
-		writeReg(0x71, 0x01);
-		writeReg(0x72, 0xFE);
-		writeReg(0x76, 0x00);
-		writeReg(0x77, 0x00);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x0D, 0x01);
-
-		writeReg(0xFF, 0x00);
-		writeReg(0x80, 0x01);
-		writeReg(0x01, 0xF8);
-
-		writeReg(0xFF, 0x01);
-		writeReg(0x8E, 0x01);
-		writeReg(0x00, 0x01);
-		writeReg(0xFF, 0x00);
-		writeReg(0x80, 0x00);
-
-		writeReg(SYSTEM_INTERRUPT_CONFIG_GPIO, 0x04);
-		writeReg(GPIO_HV_MUX_ACTIVE_HIGH, readReg(GPIO_HV_MUX_ACTIVE_HIGH) & ~0x10); // active low
-		writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
-
-		measurement_timing_budget_us = getMeasurementTimingBudget();
-
-		// "Disable MSRC and TCC by default"
-		// MSRC = Minimum Signal Rate Check
-		// TCC = Target CentreCheck
-		// -- VL53L0X_SetSequenceStepEnable() begin
-
-		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xE8);
-
-		// -- VL53L0X_SetSequenceStepEnable() end
-
-		// "Recalculate timing budget"
-		setMeasurementTimingBudget(measurement_timing_budget_us);
-
-		// VL53L0X_StaticInit() end
-
-		// VL53L0X_PerformRefCalibration() begin (VL53L0X_perform_ref_calibration())
-
-		// -- VL53L0X_perform_vhv_calibration() begin
-
-		writeReg(SYSTEM_SEQUENCE_CONFIG, 0x01);
-		if (!performSingleRefCalibration(0x40)) {
-			DEVICE_LOG("(%dHz) with address 0x%02x performSingleRefCalibration failed", (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
-			return false;
-		}
-
-		// -- VL53L0X_perform_vhv_calibration() end
-
-		// -- VL53L0X_perform_phase_calibration() begin
-
-		writeReg(SYSTEM_SEQUENCE_CONFIG, 0x02);
-		if (!performSingleRefCalibration(0x00)) {
-			DEVICE_LOG("(%dHz) with address 0x%02x performSingleRefCalibration failed ", (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
-			return false;
-		}
-
-		// -- VL53L0X_perform_phase_calibration() end
-
-		// "restore the previous Sequence Config"
-		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xE8);
-
-		DEVICE_LOG("(%dm %dHz) with address 0x%02x found", (int)_max_distance, (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
-		// VL53L0X_PerformRefCalibration() end
-
-		ret = PX4_OK;
-	}
 	return ret;
 }
 
@@ -924,7 +723,6 @@ uint8_t VL53L0X::getVcselPulsePeriod(vcselPeriodType type) {
 	else return 255;
 }
 
-
 bool VL53L0X::setVcselPulsePeriod(vcselPeriodType type, uint8_t period_pclks) {
 
 	uint8_t vcsel_period_reg = encodeVcselPeriod(period_pclks);
@@ -1090,7 +888,6 @@ bool VL53L0X::setVcselPulsePeriod(vcselPeriodType type, uint8_t period_pclks) {
 	  return true;
 }
 
-
 uint32_t VL53L0X::timeoutMclksToMicroseconds(uint16_t timeout_period_mclks, uint8_t vcsel_period_pclks) {
 	uint32_t macro_period_ns = calcMacroPeriod(vcsel_period_pclks);
 	return ((timeout_period_mclks * macro_period_ns) + (macro_period_ns / 2)) / 1000;
@@ -1152,7 +949,212 @@ bool VL53L0X::performSingleRefCalibration(uint8_t vhv_init_byte) {
 int
 VL53L0X::probe()
 {
-	return measure();
+	// read WHO_AM_I register
+	uint8_t I_AM = readReg(VL53L0X_WHO_AM_I);
+	if(I_AM == 0xEE) {
+		// "Set I2C standard mode"
+		writeReg(0x88, 0x00);
+		writeReg(0x80, 0x01);
+		writeReg(0xFF, 0x01);
+		writeReg(0x00, 0x00);
+		stop_variable = readReg(0x91);
+		writeReg(0x01, 0x00);
+		writeReg(0xFF, 0x00);
+		writeReg(0x80, 0x00);
+
+		// disable SIGNAL_RATE_MSRC (bit 1) and SIGNAL_RATE_PRE_RANGE (bit 4) limit checks
+		writeReg(MSRC_CONFIG_CONTROL, readReg(MSRC_CONFIG_CONTROL) | 0x12);
+
+		setSignalRateLimit(0.1);
+		setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+		setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+
+		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xFF);
+
+		uint8_t spad_count;
+		bool spad_type_is_aperture;
+		if (!getSpadInfo(&spad_count, &spad_type_is_aperture)) {
+			printf("No spad info\n");
+			return false;
+		}
+
+		// The SPAD map (RefGoodSpadMap) is read by VL53L0X_get_info_from_device() in
+		// the API, but the same data seems to be more easily readable from
+		// GLOBAL_CONFIG_SPAD_ENABLES_REF_0 through _6, so read it from there
+		uint8_t ref_spad_map[6];
+		readMulti(GLOBAL_CONFIG_SPAD_ENABLES_REF_0, ref_spad_map, 6);
+
+		// -- VL53L0X_set_reference_spads() begin (assume NVM values are valid)
+
+		writeReg(0xFF, 0x01);
+		writeReg(DYNAMIC_SPAD_REF_EN_START_OFFSET, 0x00);
+		writeReg(DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD, 0x2C);
+		writeReg(0xFF, 0x00);
+		writeReg(GLOBAL_CONFIG_REF_EN_START_SELECT, 0xB4);
+
+		uint8_t first_spad_to_enable = spad_type_is_aperture ? 12 : 0; // 12 is the first aperture spad
+		uint8_t spads_enabled = 0;
+
+		for (uint8_t i = 0; i < 48; i++) {
+			if (i < first_spad_to_enable || spads_enabled == spad_count) {
+		      // This bit is lower than the first one that should be enabled, or
+		      // (reference_spad_count) bits have already been enabled, so zero this bit
+		      ref_spad_map[i / 8] &= ~(1 << (i % 8));
+		    }
+		    else if ((ref_spad_map[i / 8] >> (i % 8)) & 0x1)
+		    {
+		      spads_enabled++;
+		    }
+		}
+
+		writeMulti(GLOBAL_CONFIG_SPAD_ENABLES_REF_0, ref_spad_map, 6);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x00, 0x00);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x09, 0x00);
+		writeReg(0x10, 0x00);
+		writeReg(0x11, 0x00);
+
+		writeReg(0x24, 0x01);
+		writeReg(0x25, 0xFF);
+		writeReg(0x75, 0x00);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x4E, 0x2C);
+		writeReg(0x48, 0x00);
+		writeReg(0x30, 0x20);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x30, 0x09);
+		writeReg(0x54, 0x00);
+		writeReg(0x31, 0x04);
+		writeReg(0x32, 0x03);
+		writeReg(0x40, 0x83);
+		writeReg(0x46, 0x25);
+		writeReg(0x60, 0x00);
+		writeReg(0x27, 0x00);
+		writeReg(0x50, 0x06);
+		writeReg(0x51, 0x00);
+		writeReg(0x52, 0x96);
+		writeReg(0x56, 0x08);
+		writeReg(0x57, 0x30);
+		writeReg(0x61, 0x00);
+		writeReg(0x62, 0x00);
+		writeReg(0x64, 0x00);
+		writeReg(0x65, 0x00);
+		writeReg(0x66, 0xA0);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x22, 0x32);
+		writeReg(0x47, 0x14);
+		writeReg(0x49, 0xFF);
+		writeReg(0x4A, 0x00);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x7A, 0x0A);
+		writeReg(0x7B, 0x00);
+		writeReg(0x78, 0x21);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x23, 0x34);
+		writeReg(0x42, 0x00);
+		writeReg(0x44, 0xFF);
+		writeReg(0x45, 0x26);
+		writeReg(0x46, 0x05);
+		writeReg(0x40, 0x40);
+		writeReg(0x0E, 0x06);
+		writeReg(0x20, 0x1A);
+		writeReg(0x43, 0x40);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x34, 0x03);
+		writeReg(0x35, 0x44);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x31, 0x04);
+		writeReg(0x4B, 0x09);
+		writeReg(0x4C, 0x05);
+		writeReg(0x4D, 0x04);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x44, 0x00);
+		writeReg(0x45, 0x20);
+		writeReg(0x47, 0x08);
+		writeReg(0x48, 0x28);
+		writeReg(0x67, 0x00);
+		writeReg(0x70, 0x04);
+		writeReg(0x71, 0x01);
+		writeReg(0x72, 0xFE);
+		writeReg(0x76, 0x00);
+		writeReg(0x77, 0x00);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x0D, 0x01);
+
+		writeReg(0xFF, 0x00);
+		writeReg(0x80, 0x01);
+		writeReg(0x01, 0xF8);
+
+		writeReg(0xFF, 0x01);
+		writeReg(0x8E, 0x01);
+		writeReg(0x00, 0x01);
+		writeReg(0xFF, 0x00);
+		writeReg(0x80, 0x00);
+
+		writeReg(SYSTEM_INTERRUPT_CONFIG_GPIO, 0x04);
+		writeReg(GPIO_HV_MUX_ACTIVE_HIGH, readReg(GPIO_HV_MUX_ACTIVE_HIGH) & ~0x10); // active low
+		writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+
+		measurement_timing_budget_us = getMeasurementTimingBudget();
+
+		// "Disable MSRC and TCC by default"
+		// MSRC = Minimum Signal Rate Check
+		// TCC = Target CentreCheck
+		// -- VL53L0X_SetSequenceStepEnable() begin
+
+		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xE8);
+
+		// -- VL53L0X_SetSequenceStepEnable() end
+
+		// "Recalculate timing budget"
+		setMeasurementTimingBudget(measurement_timing_budget_us);
+
+		// VL53L0X_StaticInit() end
+
+		// VL53L0X_PerformRefCalibration() begin (VL53L0X_perform_ref_calibration())
+
+		// -- VL53L0X_perform_vhv_calibration() begin
+
+		writeReg(SYSTEM_SEQUENCE_CONFIG, 0x01);
+		if (!performSingleRefCalibration(0x40)) {
+			printf("1\n");
+			DEVICE_LOG("(%dHz) with address 0x%02x performSingleRefCalibration failed", (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
+			return false;
+		}
+
+		// -- VL53L0X_perform_vhv_calibration() end
+
+		// -- VL53L0X_perform_phase_calibration() begin
+
+		writeReg(SYSTEM_SEQUENCE_CONFIG, 0x02);
+		if (!performSingleRefCalibration(0x00)) {
+			printf("2\n");
+			DEVICE_LOG("(%dHz) with address 0x%02x performSingleRefCalibration failed ", (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
+			return false;
+		}
+
+		// -- VL53L0X_perform_phase_calibration() end
+
+		// "restore the previous Sequence Config"
+		writeReg(SYSTEM_SEQUENCE_CONFIG, 0xE8);
+
+		DEVICE_LOG("(%dm %dHz) with address 0x%02x found", (int)_max_distance, (int)(1e6f / _conversion_interval), VL53L0X_BASEADDR);
+		// VL53L0X_PerformRefCalibration() end
+		return measure();
+	}
+	return -EIO;
 }
 
 void
@@ -1348,10 +1350,6 @@ VL53L0X::read(struct file *filp, char *buffer, size_t buflen)
 int
 VL53L0X::measure()
 {
-	/*
-	 * this is stub, at least for now
-	 */
-
 	return OK;
 }
 
