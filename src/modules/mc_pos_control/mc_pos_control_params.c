3562fc48fcf1d37e5c29a655464f6b7726080f20
Andrey Korigodsky <akorigod@gmail.com>
2017-11-08 23:12:29 +0300

    Replace takeoff velocity ramping with acc limit

--- src/modules/mc_pos_control/mc_pos_control_params.c
+++ src/modules/mc_pos_control/mc_pos_control_params.c
@@ -413,6 +413,18 @@ PARAM_DEFINE_FLOAT(MPC_VELD_LP, 5.0f);
 PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 10.0f);
 
 /**
+ * Maximum acceleration for takeoff
+ *
+ * @unit m/s/s
+ * @min 0.5
+ * @max 15
+ * @increment 0.1
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(MPC_ACC_TKO_MAX, 2.5f);
+
+/**
  * Acceleration for auto and for manual
  *
  * @unit m/s/s
