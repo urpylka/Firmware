f5b3d4db36655290d9a92c50a564f83c78a078b6
Oleg Kalachev <okalachev@gmail.com>
2018-03-20 23:54:38 +0300

    Disable Optical Flow module

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -39,7 +39,7 @@ set(config_module_list
 	#drivers/protocol_splitter
 	drivers/pwm_input
 	#drivers/pwm_out_sim
-	drivers/px4flow
+	#drivers/px4flow
 	drivers/px4fmu
 	drivers/px4io
 	drivers/rgbled
