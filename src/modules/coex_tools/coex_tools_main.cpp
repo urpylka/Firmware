d51d397542a25af8cb4673af7c5723fcba4e899f
Oleg Kalachev <okalachev@gmail.com>
2018-07-04 06:23:39 +0300

    Move CS HEARTBEATs monitoring to coex_tools

--- src/modules/coex_tools/coex_tools_main.cpp
+++ src/modules/coex_tools/coex_tools_main.cpp
@@ -16,6 +16,7 @@
 
 // uORB Publications
 #include <uORB/Publication.hpp>
+#include <uORB/topics/charging_station_state.h>
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/external_vehicle_position.h>
 
@@ -25,6 +26,8 @@ static orb_advert_t mavlink_log_pub;
 
 extern "C" __EXPORT int coex_tools_main(int argc, char *argv[]);
 int charging_station_distance_monitor_thread(int argc, char *argv[]);
+const char *cs_state_str(uint32_t custom_mode);
+int charging_station_state_monitor_thread(int argc, char *argv[]);
 
 int charging_station_distance_monitor_thread(int argc, char *argv[]) {
 	PX4_INFO("Starting charging station distance monitor");
@@ -57,12 +60,45 @@ int charging_station_distance_monitor_thread(int argc, char *argv[]) {
 		sprintf(buf, "CS %d distance: %g m", cs_pos.id, (double)dist);
 		mavlink_log_info(&mavlink_log_pub, buf);
 	}
+}
 
+const char *cs_state_str(uint32_t custom_mode) {
+	// Get string representation of CS state
+	switch (custom_mode) {
+		case charging_station_state_s::CUSTOM_MODE_UNKNOWN: return "UNKNOWN";
+		case charging_station_state_s::CUSTOM_MODE_OPEN: return "OPEN";
+		case charging_station_state_s::CUSTOM_MODE_OPENING: return "OPENING";
+		case charging_station_state_s::CUSTOM_MODE_CLOSED: return "CLOSED";
+		case charging_station_state_s::CUSTOM_MODE_CLOSING: return "CLOSING";
+	}
+	return "";
+}
+
+int charging_station_state_monitor_thread(int argc, char *argv[]) {
+	PX4_INFO("Starting charging station state monitor");
+
+	int state_sub = orb_subscribe(ORB_ID(charging_station_state));
+	charging_station_state_s state;
+	px4_pollfd_struct_t fds[1] = {};
+	fds[0].fd = state_sub;
+	fds[0].events = POLLIN;
+
+	while (true) {
+		int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 10000);
+		if (pret <= 0) {
+			continue;
+		}
+		orb_copy(ORB_ID(charging_station_state), state_sub, &state);
+
+		char buf[50];
+		sprintf(buf, "CS %d state: %s (%d)", state.id, cs_state_str(state.custom_mode), state.custom_mode);
+		mavlink_log_info(&mavlink_log_pub, buf);
+	}
 }
 
 int coex_tools_main(int argc, char *argv[]) {
 	if (argc < 2) {
-		PX4_INFO("usage: coex_tools {cs_dist_mon}");
+		PX4_INFO("usage: coex_tools {cs_dist_mon|cs_hb_mon}");
 		return 1;
 
 	} else if (strcmp(argv[1], "cs_dist_mon") == 0) {
@@ -73,6 +109,13 @@ int coex_tools_main(int argc, char *argv[]) {
 						charging_station_distance_monitor_thread,
 						nullptr);
 
+	} else if (strcmp(argv[1], "cs_hb_mon") == 0) {
+		__attribute__((unused)) px4_task_t deamon_task = px4_task_spawn_cmd("cs_hb_mon",
+						SCHED_DEFAULT,
+						1,
+						200,
+						charging_station_state_monitor_thread,
+						nullptr);
 	}
 
 	return OK;
