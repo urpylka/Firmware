d81dc18af40dcff9542f3fd74619ec6708461c3a
Oleg Kalachev <okalachev@gmail.com>
2018-07-09 19:51:57 +0300

    Tune telemetry rates a little

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2094,12 +2094,12 @@ Mavlink::task_main(int argc, char *argv[])
 
 	case MAVLINK_MODE_CONFIG:
 		// Enable a number of interesting streams we want via USB
-		configure_stream("ACTUATOR_CONTROL_TARGET0", 30.0f);
+		// configure_stream("ACTUATOR_CONTROL_TARGET0", 30.0f);
 		configure_stream("ADSB_VEHICLE");
 		configure_stream("ALTITUDE", 10.0f);
-		configure_stream("ATTITUDE", 50.0f);
-		configure_stream("ATTITUDE_TARGET", 8.0f);
-		configure_stream("ATTITUDE_QUATERNION", 50.0f);
+		configure_stream("ATTITUDE", 15.0f);
+		// configure_stream("ATTITUDE_TARGET", 8.0f);
+		// configure_stream("ATTITUDE_QUATERNION", 50.0f);
 		configure_stream("CAMERA_TRIGGER");
 		configure_stream("CAMERA_IMAGE_CAPTURED");
 		configure_stream("COLLISION");
@@ -2110,22 +2110,22 @@ Mavlink::task_main(int argc, char *argv[])
 		configure_stream("ESTIMATOR_STATUS", 5.0f);
 		configure_stream("EXTENDED_SYS_STATE", 2.0f);
 		configure_stream("GLOBAL_POSITION_INT", 10.0f);
-		configure_stream("HIGHRES_IMU", 50.0f);
+		// configure_stream("HIGHRES_IMU", 50.0f);
 		configure_stream("HOME_POSITION", 0.5f);
-		configure_stream("LOCAL_POSITION_NED", 30.0f);
+		configure_stream("LOCAL_POSITION_NED", 5.0f);
 		configure_stream("MANUAL_CONTROL", 5.0f);
 		configure_stream("NAMED_VALUE_FLOAT", 50.0f);
 		configure_stream("NAV_CONTROLLER_OUTPUT", 10.0f);
-		configure_stream("OPTICAL_FLOW_RAD", 10.0f);
+		// configure_stream("OPTICAL_FLOW_RAD", 10.0f);
 		configure_stream("PING", 1.0f);
-		configure_stream("POSITION_TARGET_GLOBAL_INT", 10.0f);
-		configure_stream("RC_CHANNELS", 10.0f);
-		configure_stream("SERVO_OUTPUT_RAW_0", 20.0f);
-		configure_stream("SERVO_OUTPUT_RAW_1", 20.0f);
+		configure_stream("POSITION_TARGET_GLOBAL_INT", 5.0f);
+		configure_stream("RC_CHANNELS", 5.0f);
+		// configure_stream("SERVO_OUTPUT_RAW_0", 20.0f);
+		// configure_stream("SERVO_OUTPUT_RAW_1", 20.0f);
 		configure_stream("SYS_STATUS", 1.0f);
 		configure_stream("SYSTEM_TIME", 1.0f);
 		configure_stream("TIMESYNC", 10.0f);
-		configure_stream("VFR_HUD", 20.0f);
+		configure_stream("VFR_HUD", 5.0f);
 		configure_stream("VISION_POSITION_ESTIMATE", 10.0f);
 		configure_stream("WIND_COV", 10.0f);
 		break;
