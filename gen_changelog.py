1a539c6fdadbc56c715a871f22e6cd4585a685d1
sfalexrog <sfalexrog@gmail.com>
2019-03-12 17:26:08 +0300

    travis: Actually update our release

--- gen_changelog.py
+++ gen_changelog.py
@@ -75,7 +75,7 @@ if upload_changelog:
     gh_body = gh_release.body
     if gh_body is None:
         gh_body = ''
-    gh_release.body = '{}\nChanges between {} and {}:\n{}'.format(gh_body, base_tag, current_tag, changelog)
-    print('New release body: {}'.format(gh_release.body))
-    gh_release.draft = True
-    gh_release.update()
+    gh_body = '{}\nChanges between {} and {}:\n{}'.format(gh_body, base_tag, current_tag, changelog)
+    print('New release body: {}'.format(gh_body))
+    gh_release.update_release(gh_release.tag_name, gh_body, draft=True, prerelease=True,
+                              tag_name=gh_release.tag_name, target_commitish=gh_release.target_commitish)
