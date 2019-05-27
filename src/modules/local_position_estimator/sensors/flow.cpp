73e8bc976fc6b34a2c5099378457881dfcebb422
Oleg Kalachev <okalachev@gmail.com>
2019-05-27 20:10:56 +0300

    lpe: disable flow fault detection

--- src/modules/local_position_estimator/sensors/flow.cpp
+++ src/modules/local_position_estimator/sensors/flow.cpp
@@ -184,18 +184,18 @@ void BlockLocalPositionEstimator::flowCorrect()
 	Matrix<float, n_y_flow, n_y_flow> S_I = inv<float, n_y_flow>(S);
 
 	// fault detection
-	float beta = (r.transpose() * (S_I * r))(0, 0);
+	// float beta = (r.transpose() * (S_I * r))(0, 0);
 
-	if (beta > BETA_TABLE[n_y_flow]) {
-		if (!(_sensorFault & SENSOR_FLOW)) {
-			mavlink_and_console_log_info(&mavlink_log_pub, "[lpe] flow fault,  beta %5.2f", double(beta));
-			_sensorFault |= SENSOR_FLOW;
-		}
+	// if (beta > BETA_TABLE[n_y_flow]) {
+	// 	if (!(_sensorFault & SENSOR_FLOW)) {
+	// 		mavlink_and_console_log_info(&mavlink_log_pub, "[lpe] flow fault,  beta %5.2f", double(beta));
+	// 		_sensorFault |= SENSOR_FLOW;
+	// 	}
 
-	} else if (_sensorFault & SENSOR_FLOW) {
-		_sensorFault &= ~SENSOR_FLOW;
-		mavlink_and_console_log_info(&mavlink_log_pub, "[lpe] flow OK");
-	}
+	// } else if (_sensorFault & SENSOR_FLOW) {
+	// 	_sensorFault &= ~SENSOR_FLOW;
+	// 	mavlink_and_console_log_info(&mavlink_log_pub, "[lpe] flow OK");
+	// }
 
 	if (!(_sensorFault & SENSOR_FLOW)) {
 		Matrix<float, n_x, n_y_flow> K =
