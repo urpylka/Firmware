d65d409330a9e0b13883096a783dff7d89acd93a
dvornikov-aa <dvornikov-aa@yandex.ru>
2020-01-31 19:58:35 +0300

    mission: Set valid flags explicitly

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -313,14 +313,12 @@ Mission::on_active()
 				if (charging_station_position.id == (int)_mission_item.params[2]) {
 					sp->current.lat = charging_station_position.lat;
 					sp->current.lon = charging_station_position.lon;
+					sp->current.valid = true;
 
-					if (charging_station_position.yaw_valid)
-					{
+					if (charging_station_position.yaw_valid) {
 						sp->current.yaw = charging_station_position.yaw;
 						sp->current.yaw_valid = true;
 					}
-
-					sp->current.valid = true;
 				}
 
 				// check are we in acceptance radius
@@ -341,7 +339,7 @@ Mission::on_active()
 			}
 			// precision landing, it's a charging station from the waypoint and the charging station yaw is valid
 			else if ((charging_station_position.id == (int)_mission_item.params[2]) &&
-					(charging_station_position.yaw_valid)) {
+					charging_station_position.yaw_valid) {
 				// replace landing target yaw with a charging station yaw
 				sp->current.yaw = charging_station_position.yaw;
 				sp->current.yaw_valid = true;
